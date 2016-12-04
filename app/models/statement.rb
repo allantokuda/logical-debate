2ff3421150dfcfffab7213597813a37ef52bd256
Allan Tokuda <praetis@gmail.com>
2016-12-04 10:31:09 -0600

    Agree or disagree indicated

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,9 +1,14 @@
 class Statement < ApplicationRecord
   belongs_to :parent_statement, class_name: "Statement", optional: true
 
-  has_many :child_statements, class_name: "Statment", foreign_key: "parent_statement_id"
+  has_many :child_statements, class_name: "Statement", foreign_key: "parent_statement_id"
 
   def has_parent?
     parent_statement.present?
+  end
+
+  def agree_disagree
+    return 'Neutral' if agree.nil?
+    agree ? 'Agree' : 'Disagree'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204160252) do
+ActiveRecord::Schema.define(version: 20161204161823) do
 
   create_table "statements", force: :cascade do |t|
     t.string   "text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161204160252) do
     t.integer  "parent_statement_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.boolean  "agree"
   end
 
 end
