0d34c40834c37a3f7e347fe86bc919e08e6aeaff
Allan Tokuda <praetis@gmail.com>
2016-12-31 01:30:31 -0600

    Argument parent/child navigation

--- spec/features/debate_spec.rb
+++ spec/features/debate_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe 'Debate' do
-  let(:user1) { FactoryGirl.create(:user) }
-  let(:user2) { FactoryGirl.create(:user) }
-  let(:argument) { FactoryGirl.create :argument, user: user2, published_at: Time.zone.now }
+  let(:user1) { FactoryGirl.create(:user, username: 'Allan') }
+  let(:user2) { FactoryGirl.create(:user, username: 'Bobby') }
+  let(:argument) { FactoryGirl.create :argument, user: user1, published_at: Time.zone.now }
+  let!(:premise) { FactoryGirl.create :premise, argument: argument }
 
   before(:each) do
     visit '/'
-    login_as user1, scope: :user
+    login_as user2, scope: :user
     visit argument_path(argument.id)
   end
 
@@ -16,6 +17,7 @@ describe 'Debate' do
     # TODO: indicate agreement, make edits
     click_button 'Save'
     expect(argument.reload.child_arguments).to be_present
+    expect(find('.argument-explanation', text: "You interpret Allan's argument as follows")).to be_present
   end
 
   it "allows a user to improve on another user's argument" do
@@ -23,5 +25,13 @@ describe 'Debate' do
     # TODO: indicate disagreement, make edits
     click_button 'Save'
     expect(argument.reload.child_arguments).to be_present
+    expect(find('.argument-explanation', text: "You interpret Allan's argument as follows")).to be_present
+  end
+
+  it 'can navigate between arguments children/parents' do
+    child_argument = FactoryGirl.create :argument, parent_argument: argument, user: user2
+    visit argument_path(child_argument)
+    click_link "Allan's argument"
+    click_link "Bobby's reply"
   end
 end
