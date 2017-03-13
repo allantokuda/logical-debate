81bf477b385595e0e54e906be2cde58578733612
Allan Tokuda <praetis@gmail.com>
2017-03-12 19:44:28 -0500

    SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'www.syllogy.org',
+    :authentication => :plain,
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
