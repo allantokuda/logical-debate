ac24e56323044350bb6afcc8731e71b0d57256bd
Allan Tokuda <praetis@gmail.com>
2017-03-12 21:23:30 -0500

    Use devise store_location to smooth login experience

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :get_agreement
+  after_action :store_location
 
   def get_agreement
     return unless request.method == 'GET'
@@ -12,6 +13,11 @@ class ApplicationController < ActionController::Base
   def accept_agreement
     cookies[:agreement] = { value: I18n.t('agreement.button'), expires: 365.days.from_now }
     redirect_to session[:target_path] || '/'
+  end
+
+  def store_location
+    return if request.method != 'GET' || request.path =~ /\/users\//
+    store_location_for(:user, request.path)
   end
 
   def current_user
