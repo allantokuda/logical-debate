e3f31039c74d28ae618608447afaeb20229d1b22
Allan Tokuda <praetis@gmail.com>
2017-03-12 21:23:50 -0500

    Sign in immediately after confirming email

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,8 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
+
   def after_confirmation_path_for(resource_name, resource)
-    root_path
+    sign_in(resource)
+    stored_location_for(resource) || root_path
   end
 end
